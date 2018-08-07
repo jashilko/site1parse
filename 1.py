5c5a2429dce92f06821032d2e61fde6316c5e340
jashilko <jashilko@gmail.com>
2018-08-07 23:15:18 +0000

    2 commit

--- 1.py
+++ 1.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 
 
-bd = requests.get('http://bdsmpeople.club/search/index.php?input_action=search&input_who1=2&input_who2=1&input_bdsm_position=1&input_age1=&input_age2=&input_country_id=3159&input_state_id=4312&input_city_id=0&input_online=on')
+bd = requests.get('/search/index.php?input_action=search&input_who1=2&input_who2=1&input_bdsm_position=1&input_age1=&input_age2=&input_country_id=3159&input_state_id=4312&input_city_id=0&input_online=on')
 bds = BeautifulSoup(bd.text, "html.parser")
 #print(bds)
 #p3 = bds.select('.personal_user_name_vip0')
